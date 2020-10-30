e8ecda3f23e149acd0b505346cbdfc5f4a418aae
Kritsatorn Saengweang <kritsatornsaengweang@MacBook-Pro-4.local>
2020-10-30 19:43:31 +0700

    s

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,15 +1,24 @@
 import React from "react";
 import "./Sidebar.css";
 import pic_me from "../assets/pic_me.jpg";
+import resume from "../assets/kritsatorn_sw.pdf";
+import { AiOutlineDownload, AiFillGithub } from "react-icons/ai";
+
 const Sidebar = () => {
   return (
     <>
       <img src={pic_me} alt="avatar" className="sidebar__avatar" />
 
       <div className="sidebar__name"> NAME</div>
-      <div className="sidebar__item">DOWLOAD RESUME</div>
+      <a href={resume} download="kritsatorn_sw_resume.pdf">
+        <div className="sidebar__item">
+          <AiOutlineDownload /> DOWLOAD RESUME
+        </div>
+      </a>
 
-      <div className="sidebar__item">github</div>
+      <div className="sidebar__item">
+        <AiFillGithub /> github
+      </div>
       <div className="sidebar__item">kritsatorn.sw@gmail.com</div>
       <button> email me</button>
     </>
