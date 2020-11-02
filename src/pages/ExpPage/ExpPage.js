9986d4be733cb1a5618505605883e0b66edda573
Kritsatorn Saengweang <kritsatornsaengweang@MacBook-Pro-4.local>
2020-11-02 14:01:10 +0700

    add exp

--- src/pages/ExpPage/ExpPage.js
+++ src/pages/ExpPage/ExpPage.js
@@ -1,11 +1,55 @@
 import React from "react";
-
+import "./ExpPage.css";
+import techjam__pic from "../../assets/techjam.webp";
+import aiat__pic from "../../assets/aiat.png";
 const ExpPage = () => {
   return (
-    <div>
-      EXP
-      <span>Page</span>
-    </div>
+    <>
+      <div className="d-lg-none exp__navheader" />
+      <div className="exp__body">
+        <div className="exp__title">EXPERIENCE & COMPETITION</div>
+      </div>
+      <div className="exp__section container">
+        <div className="exp__box-year">
+          <span>My journey</span>
+        </div>
+
+        <div className="exp__boxs row">
+          <div className="col-md-5 offset-md-2 boxx">
+            <div className="exp__box  ">
+              <div className="exp__pic">
+                <img src={techjam__pic} alt="techjam" />
+              </div>
+              <div>Final round - Techjam 2019</div>
+            </div>
+          </div>
+          <div className="col-md-5 offset-md-2 boxx">
+            <div className="exp__box  ">
+              <div className="exp__pic">
+                <img src={aiat__pic} alt="techjam" />
+              </div>
+              <div>Final round - Techjam 2019</div>
+            </div>
+          </div>
+          <div className="col-md-5 offset-md-2 boxx">
+            <div className="exp__box  ">
+              <div className="exp__pic">
+                <img src={techjam__pic} alt="techjam" />
+              </div>
+              <div>Final round - Techjam 2019</div>
+            </div>
+          </div>
+          <div className="col-md-5 offset-md-2 boxx">
+            <div className="exp__box  ">
+              <div className="exp__pic">
+                <img src={techjam__pic} alt="techjam" />
+              </div>
+              <div>Final round - Techjam 2019</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
